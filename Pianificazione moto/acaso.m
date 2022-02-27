6c56186afd29e3a01b4e490b33e99ad263e1ceb1
redsIng <80847977+redsIng@users.noreply.github.com>
2022-02-27 17:39:02 +0100

    up

--- Pianificazione moto/acaso.m
+++ Pianificazione moto/acaso.m
@@ -9,37 +9,79 @@ cellsize=div(end-2);
 robotsize=2;
 grid=ones(dim);
 for i=1:size(obs,1)
-                x=obs(i,1);
-                y=obs(i,2);
-                r=obs(i,3);
-                grid(x,y)=1;
-                grid(fix(-r/2)+x-1:fix(r/2)+x+1,fix(-r/2)+y-1:fix(r/2)+y+1)=0;
+    x=obs(i,1);
+    y=obs(i,2);
+    r=obs(i,3);
+    grid(x,y)=1;
+    grid(fix(-r/2)+x-1:fix(r/2)+x+1,fix(-r/2)+y-1:fix(r/2)+y+1)=0;
 end
 figure,imshow(grid);
-fun=@choice;
-S=qtdecomp(grid,@(matrix)choice({matrix,robotsize}));
-
-
+fun=@(m,s)choice(m,s);
+% @(matrix,dim)choice(matrix,dim)
+S=qtdecomp(grid,fun,robotsize);
 % S=qtdecomp(grid,0.1,robotsize);
-% blocks = repmat(uint8(1),size(S));
-% 
-% for dim = [ 64 32 16 8 4 2]
-%   numblocks = length(find(S==dim));    
-%   if (numblocks > 0)        
-%     values = repmat(uint8(1),[dim dim numblocks]);
-%     values(2:dim,2:dim,:) = 0;
-%     blocks = qtsetblk(blocks,S,dim,values);
-%   end
-% end
-% 
-% blocks(end,1:end) = 1;
-% blocks(1:end,end) = 1;
-% grid=imcomplement(grid);
-% blocks=imcomplement(blocks);
-% for i=1:size(obs,1)
-%                 x=obs(i,1);
-%                 y=obs(i,2);
-%                 r=obs(i,3);
-%                 blocks(fix(-r/2)+x-1:fix(r/2)+x+1,fix(-r/2)+y-1:fix(r/2)+y+1)=254;
-% end
-% figure,imshow(grid), figure, imshow(blocks,[])
+blocks = true(size(S));
+
+for dim = [ 64 32 16 8 4 2]
+    numblocks = length(find(S==dim));
+    if (numblocks > 0)
+        values = repmat(uint8(1),[dim dim numblocks]);
+        values(2:dim,2:dim,:) = 0;
+        blocks = qtsetblk(blocks,S,dim,values);
+        
+    end
+end
+blocks(end,1:end) = 1;
+blocks(1:end,end) = 1;
+blocks=imcomplement(blocks);
+for i=1:size(obs,1)
+    x=obs(i,1);
+    y=obs(i,2);
+    r=obs(i,3);
+    blocks(fix(-r/2)+x-1:fix(r/2)+x+1,fix(-r/2)+y-1:fix(r/2)+y+1)=0;
+end
+figure, imshow(blocks,[])
+%% Filling Holes
+for dim=[64 32 16 8 4 2]
+    [vals,r,c]=qtgetblk(blocks,S,4);
+    for i=1:size(r,1)
+        x=r(i);
+        y=c(i);
+        offsetX=x+size(vals(:,:,i),1)-1;
+        offsetY=y+size(vals(:,:,i),2)-1;
+        square=getIdx(x,y,offsetX,offsetY);
+            if ~isempty(find(ismember(double(blocks(x+1:offsetX-2,y+1:offsetY-2)),1), 1))
+                blocks(x:offsetX-2,y:offsetY-2)=zeros(size(offsetX-x,offsetY-y));
+
+            end
+        
+    end
+end
+figure, imshow(blocks,[])
+
+
+
+
+
+%%
+function idx=getIdx(x,y,offsetX,offsetY)
+idx=double.empty(0,2);
+for i=x:offsetX
+   idx=vertcat(idx,[i*ones(1,offsetX-x+1);y:offsetY]');
+end
+end
+
+%% Centroid
+% bc=regionprops(blocks,'centroid','FilledImage');
+%
+% centroids = cat(1,bc.Centroid);
+% figure
+% imshow(blocks)
+% hold on
+% plot(centroids(:,1),centroids(:,2),'b*')
+% hold off
+% %%
+% J=fill(double(blocks),'holes');
+% figure
+% imshow(J)
+
--- Pianificazione moto/choice.m
+++ Pianificazione moto/choice.m
@@ -1,15 +1,39 @@
-function logical = choice(m)
-robsize=m{2}(1);
-matrix=m{1};
-dim=size(matrix);
-if dim(1)<=2
-    logical=0;
-elseif matrix==ones(dim)
-    logical=0;
-elseif matrix==zeros()
-    logical=0;
-elseif ismember(1,matrix) && sqrt(dim(1))>=robsize
-    logical=1;
+function toSplit = choice(m,s)
+k=size(m,3);
+logical=zeros(k,1);
+for i=1:k
+    blk=m(:,:,i);
+    dim=size(blk);
+    if dim(1)<=2
+        logical(i,1)=0;
+    elseif blk==ones(dim)
+        logical(i,1)=0;
+    elseif blk==zeros(dim)
+        logical(i,1)=0;
+    elseif  findSubmat(blk,1,s)
+        logical(i,1)=1;
+    end
 end
+toSplit=find(logical==1);
 end
 
+function val=findSubmat(mat,value,s)
+A=mat;
+B=value*ones(s,s);
+szA=size(A);
+szB=size(B);
+szS=szA-szB+1;
+tf=false(szA);
+for r=1:szS(1)
+    for c=1:szS(2)
+        tf(r,c)= isequal(A(r:r+szB(1)-1,c:c+szB(2)-1),B) ;
+    end
+end
+[rout,cout]=find(tf);
+coords=[rout,cout];
+if ~isempty(coords)
+    val=1;
+else
+    val=0;
+end
+end
